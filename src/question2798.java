6c6320cb76d35bb8d950268063b57b4cb7da4526
SisypheanHUS <darkskyqa@gmail.com>
2024-03-04 22:03:46 +0700

    question2798 on leetcode in 04/03/2024

---
+++ src/question2798.java
@@ -0,0 +1,9 @@
+public class question2798 {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int count = 0;
+        for(int num : hours){
+            if(num>= target) count++;
+        }
+        return count;
+    }
+}
