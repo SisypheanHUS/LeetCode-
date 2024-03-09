c3678bd84fe54d15dd18f35d6dd4c23ce9ae8428
SisypheanHUS <darkskyqa@gmail.com>
2024-03-09 12:08:10 +0700

    question2937 on leetcode☺         in  09032024

--- src/question2951.java
+++ src/question2951.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class question2951 {
     public List<Integer> findPeaks(int[] mountain) {
         List<Integer> peaks = new ArrayList<>();
@@ -7,5 +10,11 @@ public class question2951 {
             }
         }
         return peaks;
+    }
+
+    public static void main(String[] args) {
+        question2951 q = new question2951();
+        int[] mountain = {1, 3, 2, 4, 1, 5, 6, 3, 2};
+        System.out.println(q.findPeaks(mountain));
     }
 }
