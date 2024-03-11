33c6bff08e9b419aab62e978d719511c49787c61
SisypheanHUS <darkskyqa@gmail.com>
2024-03-11 12:58:46 +0700

    2 question on leetcode in  11/03/2024 (contains daily question too)

---
+++ src/question69.java
@@ -0,0 +1,9 @@
+public class question69 {
+    public static int mySqrt(int x) {
+        return (int)Math.sqrt((double)(x));
+    }
+    public static void main(String[] args) {
+        int x =8;
+        System.out.println(mySqrt(x));
+    }
+}
