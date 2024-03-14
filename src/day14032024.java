ee53d16f075b4f70d872e1b015f9a482c511c4a1
SisypheanHUS <darkskyqa@gmail.com>
2024-03-14 15:10:57 +0700

    daily leetcode question on 14032024

---
+++ src/day14032024.java
@@ -0,0 +1,17 @@
+public class day14032024 {
+
+        public int numSubarraysWithSum(int[] nums, int goal) {
+            int count = 0;
+            int sum;
+            for(int i =0;i<nums.length;i++){
+                sum = nums[i];
+                if(sum == goal) count++;
+                for(int j = i+1;j<nums.length;j++){
+                    sum+=nums[j];
+                    if(sum == goal) count++;
+                }
+            }
+            return count;
+        }
+
+}
