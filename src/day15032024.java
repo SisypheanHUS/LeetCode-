d565776269b1fbe6772ef10c1ebcc609d51c9064
SisypheanHUS <darkskyqa@gmail.com>
2024-03-16 16:22:29 +0700

    daily question day 15032024

---
+++ src/day15032024.java
@@ -0,0 +1,19 @@
+public class day15032024 {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        int[] left = new int[nums.length];
+        int[] right = new int[nums.length];
+        left[0] = 1;
+        right[nums.length-1] =1;
+        for(int i = 1;i<nums.length;i++){
+            left[i] = nums[i-1]*left[i-1];
+        }
+        for(int i = nums.length - 2;i >= 0;i--){
+            right[i] = nums[i+1]*right[i+1];
+        }
+        for(int i= 0;i<nums.length;i++){
+            res[i] = left[i]*right[i];
+        }
+        return res;
+    }
+}
