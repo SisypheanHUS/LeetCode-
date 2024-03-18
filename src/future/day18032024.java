4a55c9244bd9b26fb1f13d2a1e2a61268d1a6eaa
SisypheanHUS <darkskyqa@gmail.com>
2024-03-19 00:46:55 +0700

    update

--- src/future/day18032024.java
+++ src/future/day18032024.java
@@ -1,4 +1,20 @@
 package future;
 
+import java.util.Arrays;
+
 public class day18032024 {
+    public int findMinArrowShots(int[][] points) {
+        int count = 0;
+        Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+        int i = 0;
+        while (i < points.length) {
+            int end = points[i][1];
+            i++;
+            while (i < points.length && points[i][0] <= end) {
+                i++;
+            }
+            count++;
+        }
+        return count;
+    }
 }
